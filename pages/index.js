6e418806c91258a05f27636ac4b2aebc41dc15f1
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2024-01-08 23:14:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,19 @@ import { fadeIn } from '../variants';
 
 const Home = () => {
   return (
+
+    
     <div className="h-full bg-primary/60 ">
+
+       {/* particles */}
+       <ParticlesContainer />
+       
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="container flex flex-col justify-center h-full mx-auto text-center xl:pt-40 xl:text-left">
           {/* title */}
-          <motion.h1 variants={fadeIn('down', 0.2)}
+          <motion.h1 
+            variants={fadeIn('down', 0.2)}
             initial="hidden"
             animate="show"
             exit={"hidden"}
@@ -57,14 +64,23 @@ const Home = () => {
 
       {/* image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0 " >
+       
         {/* bg image */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"> </div>
-        {/* particles */}
-        <div>particles</div>
+        
+        {/* particles commented to full screen
+       <ParticlesContainer /> */}
+       
         {/* avatar img */}
-        <div className="w-full h-full max-w-[737px] max-h-[678] absolute -bottom-32 lg:bottom-[-22%] lg:right-[-0%] ">
+        <motion.div 
+            variants={fadeIn('up', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit={"hidden"} 
+            transition={ {duration: 1, ease: 'easeInOut'}}
+            className="w-full h-full max-w-[737px] max-h-[678] absolute -bottom-32 lg:bottom-[-22%] lg:right-[-0%] ">
           <Avatar/>
-        </div>
+        </motion.div>
       </div>
 
     </div>
