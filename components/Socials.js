5a8573f76825d30bd51a7ed11a8455fc9463af50
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2024-01-12 02:34:18 +0530

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,38 @@
+// Links
+import Link from "next/link";
+
+//icon
+import {
+   RiYoutubeLine, 
+   RiInstagramLine,
+   RiFacebookLine, 
+   RiGithubLine, 
+   RiLinkedinLine   
+  } from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (<div className="flex items-center gap-5 text-xl" >
+    
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiInstagramLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiFacebookLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiGithubLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiLinkedinLine />
+    </Link>
+
+    
+    
+  </div>
+  );
 };
 
 export default Socials;
