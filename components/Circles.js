4a4dc89a82dee6e7e01335252a31ae7fdead50cd
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2024-01-23 01:15:47 +0530

    Update Circles.js

--- components/Circles.js
+++ components/Circles.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[200px] absolute -right-11 -bottom-11 mix-blend-color-dodge animate-pulse duration-75 z-10">
+    <div className="w-[200px] xl:w-[200px] absolute -right-10 -bottom-10 mix-blend-color-dodge animate-pulse duration-75 z-10">
       <Image
         src={'/circles.png'}
         width={260}
