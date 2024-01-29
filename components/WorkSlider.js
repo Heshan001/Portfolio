a85455dcc3a2fcbf7a755df6298a51e8a05615c0
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2024-01-30 04:41:37 +0530

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,14 @@
-// data
-const workSlides = {
+// Import necessary components and styles
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
+import Image from 'next/image';
+import { BsArrowRight } from 'react-icons/bs';
+
+// Data
+export const workSlides = {
   slides: [
     {
       images: [
@@ -45,7 +54,46 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[480px]'
+    >
+      {workSlides.slides.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+            {slide.images.map((image, index) => (
+              <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
+                <div className='flex items-center justify-center relative overflow-hidden group'>
+                  {/* image */}
+                  <Image src={image.path} width={500} height={300} alt='' />
+                  {/* overlay gradient */}
+                  <div className=' absolute inset-0 bg-gradient-to-l 
+                   from-transparent via-[#4608ca] to-[#b62fa4e7] opacity-0 
+                   group-hover:opacity-80 transition-all duration-700'></div>
+                   {/* title */}
+                   <div className=' absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                    <div className='flex  items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                      {/* title part 1 */}
+                      <div className=' delay-100'>LIVE</div>
+                      {/* title part 2 */}
+                      <div className=' translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+                      {/* icon */}
+                      <div className=' text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight /></div>
+                    </div>
+                   </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
