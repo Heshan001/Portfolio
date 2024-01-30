2e57a6f1ee044fac24270960ab88464a56f5e046
Supun Abayarathna <120658729+Heshan001@users.noreply.github.com>
2024-01-30 07:25:12 +0530

    Create CvDownload.js

---
+++ components/CvDownload.js
@@ -0,0 +1,20 @@
+
+
+// next link
+import Link from "next/link";
+
+//icon
+import { HiArrowDown } from 'react-icons/hi2';
+
+const CvDownload = () => {
+  return <div className="  mx-auto xl:mx-0">
+    <Link a href={'https://drive.google.com/file/d/1JmPr3LvRthc4ciFN8pKFZUr-Go2HC8hm/view?usp=sharing'}
+     className=" relative w-[185px] h-[185] flex  items-center  bg-no-repeat group">   
+  
+<button className=" px-6 py-2 ml-5 mt-10 rounded-full   bg-white hover:bg-accent text-black ">Download CV</button>
+
+    </Link>
+  </div>;
+};
+
+export default CvDownload;
